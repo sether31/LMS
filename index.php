903ee03d5820eec03bb82912b93e9db06fda89dd
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-04-17 09:48:00 +0800

    chore: Add index.php in the root folder
    - To keep things clean it will direct the user to pages/auth/login.php.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+  /*
+    Ranier P. Catinza
+    Fidel Izak C. Adriano
+    John Michael Catangay
+    Seth Michael O. Hernandez
+    Marion Dominique D. Blanquisco
+    
+  */ 
+  header("Location: ./pages/authentication/login.php");
+?>
