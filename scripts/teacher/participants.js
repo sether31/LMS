33270fe7aefe68d8205b441897428098b458a191
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-05-03 01:15:47 +0800

    chore: Add style and script for participants
    - I forgot to commit.

---
+++ scripts/teacher/participants.js
@@ -0,0 +1,24 @@
+const sidebar = document.querySelector('.sidebar');
+const hamburgerBtn = document.querySelector('.sidebar-hamburger-btn');
+
+hamburgerBtn.addEventListener('click', function(){
+  this.classList.toggle('click');
+  sidebar.classList.toggle('show');
+});
+
+const moduleBtn = document.querySelectorAll('.module-btn');
+moduleBtn.forEach((btn)=>{
+  btn.addEventListener('click', ()=>{
+    const moduleContent = btn.parentElement.querySelector('.module-content');
+    const arrowContainer = btn.querySelector('span');
+  
+    moduleContent.classList.toggle('show');
+    arrowContainer.classList.toggle('rotate');
+  });
+});
+
+document.querySelectorAll('.sidebar nav ul .main-list').forEach((mainList)=>{
+  mainList.addEventListener('click', ()=>{
+    mainList.classList.toggle('active');
+  });
+});
